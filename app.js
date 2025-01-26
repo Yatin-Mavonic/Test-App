b3926e17cfe38bb6881bbb18e106793c651c62b9
Yatin-Mavonic <yatin@mavonic.com>
2025-01-26 13:01:58 +0530

    test

--- app.js
+++ app.js
@@ -14,5 +14,16 @@ app.use((req,res) => {
     res.status(400).json({error: "URL Not Found!!!"})
 })
 
+function findDuplicates(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                console.log("Duplicate found:", arr[i]);
+            }
+        }
+    }
+}
+
+
 const PORT = 3005;
 app.listen(PORT, () => console.log(`Server Started on PORT: ${PORT}`))
